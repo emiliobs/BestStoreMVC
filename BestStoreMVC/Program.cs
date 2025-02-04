cdeb0d099f92977462b89f39c2a2ee36da622a47
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-04 11:39:41 +0000

    Created DbCOntext and injection of the dbContext and more.

--- BestStoreMVC/Program.cs
+++ BestStoreMVC/Program.cs
@@ -1,7 +1,12 @@
+using BestStoreMVC.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
@@ -24,6 +29,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
---
+++ BestStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
