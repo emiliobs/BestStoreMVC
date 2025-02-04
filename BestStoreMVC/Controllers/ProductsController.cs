667586f09f6bd1e73e15fccbe976106f298b109e
Emilio Barrera <barrera_emilio@yahoo.es>
2025-02-04 13:28:51 +0000

    Createed Index View Done and more

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.OrderByDescending(p => p.Id).ToListAsync());
+        }
+    }
+}
